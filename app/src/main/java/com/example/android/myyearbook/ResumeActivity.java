065c18ceea3ff51bdcba86c504f745bb28d766a5
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-03-05 16:48:25 -0500

    Added a WebView to the Resume activity

--- app/src/main/java/com/example/android/myyearbook/ResumeActivity.java
+++ app/src/main/java/com/example/android/myyearbook/ResumeActivity.java
@@ -2,6 +2,7 @@ package com.example.android.myyearbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class ResumeActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class ResumeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume);
+
+        String html = "<html><body><center><h1>Testing</h1></center></body></html>";
+
+        ((WebView) findViewById(R.id.webview)).loadData(html, "text/html", null);
     }
 }
