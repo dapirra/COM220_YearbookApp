f065465c88fd9900fce018f79f3e4dc94399d05d
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-03-05 16:49:36 -0500

    Added a button to the MainActivity to start the ResumeActivity

--- app/src/main/java/com/example/android/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/android/myyearbook/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.myyearbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.resume_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ResumeActivity.class));
+            }
+        });
     }
 }
