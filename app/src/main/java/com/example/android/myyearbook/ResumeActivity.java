911ef6d349052f23f3e29d1522386e099f4a74c1
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-03-13 18:25:59 -0400

    Modified the HTML code in the WebView

--- app/src/main/java/com/example/android/myyearbook/ResumeActivity.java
+++ app/src/main/java/com/example/android/myyearbook/ResumeActivity.java
@@ -11,7 +11,18 @@ public class ResumeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume);
 
-        String html = "<html><body><center><h1>Testing</h1></center></body></html>";
+        String html = "<html><head><style>@keyframes test {\n" +
+                "from {transform: rotateY(0deg);}\n" +
+                "to {transform: rotateY(360deg);}\n" +
+                "}</style></head>" +
+                "<body><marquee>Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text </marquee>" +
+                "<br><center><br><br><br>" +
+                "<h1 style=\"animation: test 2s infinite linear; text-align: center; font-size: 40px;\">Hire me!</h1><br><br>" +
+                "<marquee direction=\"down\" width=\"260\" height=\"200\" behavior=\"alternate\" style=\"border:solid\">\n" +
+                "  <marquee behavior=\"alternate\">\n" +
+                "    Best resume right here\n" +
+                "  </marquee>\n" +
+                "</marquee></center></body></html>";
 
         ((WebView) findViewById(R.id.webview)).loadData(html, "text/html", null);
     }
