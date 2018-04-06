d02e289173555b0be1fb463873482dfb4a6d200d
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-04-06 01:01:58 -0400

    Organized Imports

--- app/src/main/java/com/example/android/myyearbook/ResumeActivity.java
+++ app/src/main/java/com/example/android/myyearbook/ResumeActivity.java
@@ -1,7 +1,7 @@
 package com.example.android.myyearbook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 
 public class ResumeActivity extends AppCompatActivity {
--- app/src/main/java/com/example/android/myyearbook/WeatherActivity.java
+++ app/src/main/java/com/example/android/myyearbook/WeatherActivity.java
@@ -1,8 +1,8 @@
 package com.example.android.myyearbook;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import java.util.ArrayList;
