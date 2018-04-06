42536a9ad02de6033fac28de31734d2a326874ce
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-04-06 00:55:32 -0400

    Removed incomplete load and save methods for now

--- app/src/main/java/com/example/android/myyearbook/WeatherActivity.java
+++ app/src/main/java/com/example/android/myyearbook/WeatherActivity.java
@@ -1,24 +1,16 @@
 package com.example.android.myyearbook;
 
-import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class WeatherActivity extends AppCompatActivity {
 
     final static String WEATHER_API_URL =
             "https://api.darksky.net/forecast/115983dea73677f9a74319ccfd8a9c81/40.7657,-73.0152?exclude=minutely,hourly,alerts,flags";
-
-    final static String FILE_NAME = "Weather.json";
 
     private WeatherAdapter weatherAdapter;
 
@@ -41,7 +33,6 @@ public class WeatherActivity extends AppCompatActivity {
         protected void onPostExecute(ArrayList<Weather> result) {
             // Update the information displayed to the user.
             updateUi(result);
-            saveData();
         }
     }
 
@@ -51,30 +42,6 @@ public class WeatherActivity extends AppCompatActivity {
             ((ListView) findViewById(R.id.weather_list)).setAdapter(weatherAdapter);
         } else {
             weatherAdapter.notifyDataSetChanged();
-        }
-    }
-
-    // Incomplete
-    private void loadData() {
-        try {
-            BufferedReader inputStream = new BufferedReader(
-                    new InputStreamReader(openFileInput(FILE_NAME), "UTF-8"));
-            updateUi(Utils.extractWeatherFromJson(inputStream.readLine()));
-            inputStream.close();
-        } catch (FileNotFoundException e) {
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Save the JSON data to the save file
-    private void saveData() {
-        try {
-            FileOutputStream outputStream = openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
-            outputStream.write(Utils.jsonWeather.getBytes()); // Convert to binary and write
-            outputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
